0b710c32c6f6bec0e4701076de6a480b802cf833
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2020-07-08 16:22:19 +0530

    irst Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBsNLqOsQ_qWiHiZnfRN3ek7ehfs6nKwuU",
+    authDomain: "estaff-3b51b.firebaseapp.com",
+    databaseURL: "https://estaff-3b51b.firebaseio.com",
+    projectId: "estaff-3b51b",
+    storageBucket: "estaff-3b51b.appspot.com",
+    messagingSenderId: "1043619781708",
+    appId: "1:1043619781708:web:ae833883eb40a914bde8e9",
+    measurementId: "G-YBC3W1LT22"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBsNLqOsQ_qWiHiZnfRN3ek7ehfs6nKwuU",
+    authDomain: "estaff-3b51b.firebaseapp.com",
+    databaseURL: "https://estaff-3b51b.firebaseio.com",
+    projectId: "estaff-3b51b",
+    storageBucket: "estaff-3b51b.appspot.com",
+    messagingSenderId: "1043619781708",
+    appId: "1:1043619781708:web:ae833883eb40a914bde8e9",
+    measurementId: "G-YBC3W1LT22"
+  }
 };
 
 /*
