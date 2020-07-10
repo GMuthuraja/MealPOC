0cc4e23af39c27cc92a14d75de30620982fdae86
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2020-07-10 23:09:17 +0530

    Chnages made

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBsNLqOsQ_qWiHiZnfRN3ek7ehfs6nKwuU",
-    authDomain: "estaff-3b51b.firebaseapp.com",
-    databaseURL: "https://estaff-3b51b.firebaseio.com",
-    projectId: "estaff-3b51b",
-    storageBucket: "estaff-3b51b.appspot.com",
-    messagingSenderId: "1043619781708",
-    appId: "1:1043619781708:web:ae833883eb40a914bde8e9",
-    measurementId: "G-YBC3W1LT22"
+    apiKey: "AIzaSyBWlpIBH11c1SkvT3JfG7QX5elyWV1MMNU",
+    authDomain: "saudia-estaff-meal.firebaseapp.com",
+    databaseURL: "https://saudia-estaff-meal.firebaseio.com",
+    projectId: "saudia-estaff-meal",
+    storageBucket: "saudia-estaff-meal.appspot.com",
+    messagingSenderId: "416324052649",
+    appId: "1:416324052649:web:dc53ac593917183c37ebb8",
+    measurementId: "G-JFYR8Q8TCK"
   }
 };
